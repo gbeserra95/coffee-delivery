eef703f78f7df2400a6ec7d1c1e537d1e8d41dec
Gabriel <gabriel.fernandesb@gmail.com>
2022-10-05 22:34:46 -0300

    style: success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,3 +1,49 @@
+import { MapPin, Timer, CurrencyDollar } from 'phosphor-react'
+import deliverySuccessImage from '../../assets/illustration.svg'
+import * as S from './styles'
+
 export function Success() {
-  return <h1>Success</h1>
+  return (
+    <main>
+      <S.Title>
+        <h1>Uhu! Pedido confirmado!</h1>
+        <span>Agora é só aguardar que logo o café chegará até você.</span>
+      </S.Title>
+      <S.Content>
+        <S.InfoContainer>
+          <S.Info>
+            <S.Icon bg="purple">
+              <MapPin size={16} />
+            </S.Icon>
+            <S.Text>
+              Entrega em <span>Rua João Daniel Martinelli, 102</span>
+              <br />
+              Farrapos - Porto Alegre, RS
+            </S.Text>
+          </S.Info>
+          <S.Info>
+            <S.Icon bg="yellow">
+              <Timer size={16} />
+            </S.Icon>
+            <S.Text>
+              Previsão de entrega
+              <br />
+              <span>20 min - 30 min</span>
+            </S.Text>
+          </S.Info>
+          <S.Info>
+            <S.Icon bg="yellowDark">
+              <CurrencyDollar size={16} />
+            </S.Icon>
+            <S.Text>
+              Pagamento na entrega
+              <br />
+              <span>Cartão de Crédito</span>
+            </S.Text>
+          </S.Info>
+        </S.InfoContainer>
+        <img src={deliverySuccessImage} alt="" />
+      </S.Content>
+    </main>
+  )
 }
---
+++ src/pages/Success/styles.ts
@@ -0,0 +1,84 @@
+import styled from 'styled-components'
+
+export const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding-top: 5rem;
+  margin-bottom: 2.5rem;
+  gap: 0.25rem;
+
+  h1 {
+    font-family: 'Baloo 2';
+    font-size: 2rem;
+    font-weight: 800;
+    line-height: 130%;
+    color: ${(props) => props.theme['yellow-dark']};
+  }
+
+  span {
+    font-size: 1.25rem;
+    line-height: 130%;
+    color: #${(props) => props.theme['base-subtitle']};
+  }
+`
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: min(100%, 32.875rem);
+  height: 16.875rem;
+  padding: 2.5rem;
+  gap: 2rem;
+  border: 1px solid transparent;
+  border-radius: 6px 36px;
+  background-image: linear-gradient(
+      ${(props) => props.theme.background},
+      ${(props) => props.theme.background}
+    ),
+    linear-gradient(45deg, rgba(219, 172, 44, 1), rgba(128, 71, 248, 1));
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+`
+
+export const Info = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+`
+
+const COLORS = {
+  yellow: 'yellow',
+  yellowDark: 'yellow-dark',
+  gray: 'base-text',
+  purple: 'purple',
+} as const
+
+interface IColors {
+  bg: keyof typeof COLORS
+}
+
+export const Icon = styled.div<IColors>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+  color: ${(props) => props.theme.white};
+  background: ${(props) => props.theme[COLORS[props.bg]]};
+`
+
+export const Text = styled.div`
+  color: ${(props) => props.theme['base-text']};
+
+  span {
+    font-weight: 700;
+  }
+`
